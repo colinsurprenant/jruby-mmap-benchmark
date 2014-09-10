334f2d077857d158312d6ba979bb3fd077efdfe0
Colin Surprenant <colin.surprenant@gmail.com>
2014-09-10 19:25:51 -0400

    cleaned up tests structure and removed non useful strategies

--- mmap_test.rb
+++ mmap_test.rb
@@ -14,8 +14,11 @@ java_import "com.colinsurprenant.MmapFile"
 
 require "mmap_file_ext"
 
+# create an alias for the MmapFile#unsafe_write method signature taking a RubyString object, this is a bit faster than using java_send
+# this is required to avoid the JRuby/Java String auto conversion and access the RubyString object from within the Java class
 class MmapFile
   java_alias :aliased_unsafe_write, :unsafe_write, [RubyString]
+  java_alias :aliased_write, :write, [RubyString]
 end
 
 module MmapTest
@@ -29,8 +32,8 @@ module MmapTest
       @buffer = @channel.map(FileChannel::MapMode::READ_WRITE, 0, size)
     end
 
-    def rewind
-      @buffer = @channel.map(FileChannel::MapMode::READ_WRITE, 0, @size)
+    def seek(pos)
+      @buffer = @channel.map(FileChannel::MapMode::READ_WRITE, pos, @size)
     end
 
     def write(data)
@@ -42,194 +45,51 @@ module MmapTest
     end
   end
 
-  BUFFER_SIZE = 1024
-  STRING_1K = ("abcdefg\n" * (BUFFER_SIZE / 8)).force_encoding(Encoding::ASCII_8BIT)
-  JAVA_STRING_1K = Java::JavaLang::String.new(STRING_1K)
-  BYTES_1K = STRING_1K.to_java_bytes
-  BUFFER_COUNT = 2 * 1000 * 1024
+  BUFFERS = {
+     1 => ("abcdefg\n" * ( 1 * 1024 / 8)).force_encoding(Encoding::ASCII_8BIT),
+     4 => ("abcdefg\n" * ( 4 * 1024 / 8)).force_encoding(Encoding::ASCII_8BIT),
+    16 => ("abcdefg\n" * (16 * 1024 / 8)).force_encoding(Encoding::ASCII_8BIT),
+  }
+
+  # I previously had tests with Java String and Java byte[] but these are not realistically usable from a JRuby
+  # context so I removed them.
+  #
+  # JAVA_STRING_1K = Java::JavaLang::String.new(STRING_1K)
+  # BYTES_1K = STRING_1K.to_java_bytes
+
   REPORT_WIDTH = 50
+  WRITE_SIZE = 2 * 1000 * 1024 * 1024
 
-  TESTS = [
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFileExt.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFileExt, unsafe, ruby String") do
-          mmaped_file.rewind
+  def self.bench(desc, writer_class, buffer, &write_block)
+    raise("invalid buffer size") if WRITE_SIZE % buffer.size != 0
+    buffer_count = WRITE_SIZE / buffer.size
+    path =  writer_class == File ? "test_data.file" : "test_data.mmap"
+    File.delete(path) rescue nil
+    out = writer_class.new(path, writer_class == File ? "w+" : WRITE_SIZE)
 
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.unsafe_write(STRING_1K)
-          end
-        end
+    Benchmark.bmbm(REPORT_WIDTH) do |b|
+      b.report(desc) do
+        out.seek(0)
+        buffer_count.times.each{|i| write_block.call(out, buffer)}
       end
-      mmaped_file.close
-    end,
+    end
+    out.close
+  end
 
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFileExt.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFileExt, ruby String") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(STRING_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = PureMmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("ruby PureMmapFile ruby String#to_java_bytes") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(STRING_1K.to_java_bytes)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = PureMmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("ruby PureMmapFile java String#get_bytes") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(JAVA_STRING_1K.get_bytes)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = PureMmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("ruby PureMmapFile java bytes") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(BYTES_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFile boxed ruby String") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(STRING_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFile, unboxed, safe, ruby String") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.java_send(:write, [RubyString], STRING_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFile, unboxed, unsafe, ruby String") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.java_send(:unsafe_write, [RubyString], STRING_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFile, alias, unboxed, unsafe, ruby String") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.aliased_unsafe_write(STRING_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFile java String") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(JAVA_STRING_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      File.delete("test_data.mmap") rescue nil
-      mmaped_file = MmapFile.new("test_data.mmap", BUFFER_COUNT * BUFFER_SIZE)
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("java MmapFile java bytes") do
-          mmaped_file.rewind
-
-          BUFFER_COUNT.times.each do |i|
-            mmaped_file.write(BYTES_1K)
-          end
-        end
-      end
-      mmaped_file.close
-    end,
-
-    lambda do
-      file = File.new("test_data.file", "w+")
-      Benchmark.bmbm(REPORT_WIDTH) do |b|
-        b.report("ruby File") do
-          file.seek(0)
-
-          BUFFER_COUNT.times.each do |i|
-            file.puts(STRING_1K)
-          end
-        end
-      end
-      file.close
-    end,
-  ]
+  TESTS = [1, 4, 16].map do |size|
+    [
+      {:desc => "ruby File, #{size}k String", :class => File, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.puts(buffer)}},
+      {:desc => "java MmapFileExt, unboxed, unsafe, #{size}k ruby String", :class => MmapFileExt, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.unsafe_write(buffer)}},
+      {:desc => "java MmapFileExt, unboxed, safe, #{size}k ruby String", :class => MmapFileExt, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.write(buffer)}},
+      {:desc => "java MmapFile, unboxed, unsafe, #{size}k ruby String", :class => MmapFile, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.aliased_unsafe_write(buffer)}},
+      {:desc => "java MmapFile, unboxed, safe, #{size}k ruby String", :class => MmapFile, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.aliased_write(buffer)}},
+      {:desc => "java MmapFile, autoboxing, #{size}k ruby String", :class => MmapFile, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.write(buffer)}},
+      {:desc => "ruby PureMmapFile, #{size}k ruby String#to_java_bytes", :class => PureMmapFile, :buffer => BUFFERS.fetch(size), :write => lambda{|out, buffer| out.write(buffer.to_java_bytes)}},
+    ]
+  end.flatten
 end
 
-MmapTest::TESTS[ARGV[0].to_i].call if __FILE__==$0
+if __FILE__ == $0
+  test = MmapTest::TESTS[ARGV[0].to_i]
+  MmapTest.bench(test[:desc], test[:class], test[:buffer], &test[:write])
+end
